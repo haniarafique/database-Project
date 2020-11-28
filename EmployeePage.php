fc66d6704ba32790bbe9e927fcfbce51bf2c8a37
haniarafique <71470869+haniarafique@users.noreply.github.com>
2020-11-29 01:55:20 +0500

    Create EmployeePage.php

---
+++ EmployeePage.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+   <head>
+    <meta charset="UTF-8">
+	<title>Employee menu</title> 
+	<link rel="stylesheet" href="./style.css">
+   </head>
+
+
+
+   <body>
+    <h1>HOTEL TRANSYLVANIA</h1>
+    <h1>EMPLOYEE PAGE</h1>
+    <div class = "s">
+    <p>
+        <form action="BookingReport.php" method="get" target="_blank">
+            <button type="submit">CHECK BOOKING </button>
+        </form>
+        <form action="GuestDetails.php" method="get" target="_blank">
+             <button type="submit">CHECK GUEST DETAILS</button>
+        </form>
+    <p>
+
+    <p>
+        <form action="https://www.w3docs.com/" method="get" target="_blank">
+            <button type="submit">CHECK IN </button>
+        </form>
+        <form action="http://localhost:8888/DbProj/login.php" method="get" target="_blank">
+             <button type="submit">CHECK OUT</button>
+        </form>
+    <p>
+
+    <p>
+        <form action="https://www.w3docs.com/" method="get" target="_blank">
+            <button type="submit">EDIT BOOKING </button>
+        </form>
+        <form action="CancelPage.php" method="get" target="_blank">
+             <button type="submit">CANCEL BOOKING</button>
+        </form>
+
+    </p>
+
+    <p>
+        <form action="https://www.w3docs.com/" method="get" target="_blank">
+            <button type="submit">REWARD CHECKING </button>
+        </form>
+        <form action="http://localhost:8888/DbProj/login.php" method="get" target="_blank">
+             <button type="submit">INVOICE GENERATION</button>
+        </form>
+
+    </p>
+
+
+    <p>
+        <form action="http://localhost:8888/DbProj/login.php" method="get" target="_blank">
+            <button type="submit">BACK TO LOGIN MENU</button>
+       </form>
+
+
+    </p>
+</div>
+<img src="https://miro.medium.com/max/2800/1*7jDLREu-uME27nRvzwckBw.jpeg">
+   </body>
+</html>
